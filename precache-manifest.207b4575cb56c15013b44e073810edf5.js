cb908127c246b38f97d1ce2a1e582b31bb0dca03
AishwaryaHase2711 <aishwaryahase1998@gmail.com>
2020-08-02 14:40:13 +0530

    static files

---
+++ precache-manifest.207b4575cb56c15013b44e073810edf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103164ef7b87266efc49a7f3f314069d",
+    "url": "/ox-game/index.html"
+  },
+  {
+    "revision": "fb71ed3d6b8aaa22b337",
+    "url": "/ox-game/static/css/main.8ce5d0a6.chunk.css"
+  },
+  {
+    "revision": "6f87e2bdd1f6fa90b5bb",
+    "url": "/ox-game/static/js/2.a741715f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ox-game/static/js/2.a741715f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb71ed3d6b8aaa22b337",
+    "url": "/ox-game/static/js/main.2ad0e341.chunk.js"
+  },
+  {
+    "revision": "9da6613ad3262c012171",
+    "url": "/ox-game/static/js/runtime-main.67ae9d5f.js"
+  }
+]);
